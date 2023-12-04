c83be951fbb579733dc3efe7f40141e8da1045e8
Lucas Alberti <93678168+lucasfranciscoalberti@users.noreply.github.com>
2023-12-04 15:24:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ sr.reveal('.container', {duration: 1300});
 
 sr.reveal('.dispositivos', {
     rotate: {x: 0, y: 80, z: 0},
-    duration: 2000});
+    duration: 1300});
     
 sr.reveal('.rodape', {duration: 1000});
