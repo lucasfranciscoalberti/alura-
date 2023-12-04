e7897c2db9dc4801a28b58d0b663e544ef600def
lucasfranciscoalberti <lucasfranciscoalberti@gmail.com>
2023-12-04 15:20:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.container', {duration: 1300});
+
+sr.reveal('.dispositivos', {
+    rotate: {x: 0, y: 80, z: 0},
+    duration: 2000});
+    
+sr.reveal('.rodape', {duration: 1000});
